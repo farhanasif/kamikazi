0eb52c4b598b346f36c3bd16338b25214edc55b4
Farhan Asif <f.asif2010@gmail.com>
2018-10-29 12:35:47 +0600

    configuring express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hi boys')
+const express = require('express')
+const app = express()
+const port = 7800
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/webhook', (req, res) => {
+    res.send('we are in webhook');
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
